f2156661b7eb0d9fe8038cad5343017ddaed1fc6
renan jegouzo <renan@aestesis.org>
2026-08-26 16:50:20 +0200

    camera ok

--- lib/modules/camera.dart
+++ lib/modules/camera.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 import '../aestesis.dart';
 import '../controls/asset.view.dart';
+import '../core/flutter.extensions.dart';
 import '../core/native.view.dart';
 import '../ui/icon.dart';
 import '../ui/menu.button.dart';
@@ -46,106 +47,158 @@ class _CameraModuleState extends State<CameraModule> {
   @override
   Widget build(BuildContext context) {
     final control = module[CameraControl.asset.id];
+    final cvalue = control.value.toInt();
+    final casset = assets.range.contains(cvalue) ? assets[cvalue] : null;
+    final cname = casset?.name;
     return Padding(
-        padding: const EdgeInsets.all(5),
-        child: Row(children: [
+      padding: const EdgeInsets.all(5),
+      child: Row(
+        children: [
           Expanded(
-              child: Column(children: [
-            SizedBox(
-                height: 20,
-                child: Row(children: [
-                  const Spacer(),
-                  UIMenuIconButton(
-                    tooltip: 'Add camera input',
-                    asset: UIIcon.add, menu: [
-                    ...devices
-                        .where((d) =>
-                            assets.firstWhereOrNull((a) => a.id == d.id) ==
-                            null)
-                        .map((device) => UIMenuItem(
-                            text: device.name,
-                            icon: device.icon,
-                            onTap: () {
-                              aes.bus.fire(AddAssetsEvent(
-                                  module: module, assets: [device.toAsset()]));
-                            }))
-                  ])
-                ])),
-            const SizedBox(height: 5),
-            Expanded(
-                child: Scrollbar(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                  child: Row(
+                    children: [
+                      const Spacer(),
+                      UIMenuIconButton(
+                        tooltip: 'Add camera input',
+                        asset: UIIcon.add,
+                        menu: [
+                          ...devices
+                              .where(
+                                (d) =>
+                                    assets.firstWhereOrNull(
+                                      (a) => a.id == d.id,
+                                    ) ==
+                                    null,
+                              )
+                              .map(
+                                (device) => UIMenuItem(
+                                  text: device.name,
+                                  icon: device.icon,
+                                  onTap: () {
+                                    aes.bus.fire(
+                                      AddAssetsEvent(
+                                        module: module,
+                                        assets: [device.toAsset()],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Expanded(
+                  child: Scrollbar(
                     controller: scrollController,
                     child: CustomScrollView(
-                        controller: scrollController,
-                        slivers: [
-                          SliverGrid.builder(
-                              itemCount: assets.length,
-                              gridDelegate:
-                                  SliverGridDelegateWithMinMaxCrossAxisExtent(
-                                      maxCrossAxisCount: assets.length,
-                                      minCrossAxisExtent: 160,
-                                      crossAxisSpacing: 10,
-                                      mainAxisSpacing: 10,
-                                      mainAxisExtent: 90),
-                              itemBuilder: (_, i) => UIContextMenu(
-                                      menu: [
-                                          UIMenuItem(
-                                              text: 'Remove camera input',
-                                              icon: UIIcon.delete,
-                                              color: Colors.redAccent,
-                                              onTap: () {
-                                                aes.bus.fire(RemoveAssetsEvent(
-                                                    module: module,
-                                                    assets: [assets[i]]));
-                                              })
-                                      ],
-                                      child: AssetView(
-                                        key:
-                                            Key('${module.id}.${assets[i].id}'),
-                                        moduleId: module.id,
-                                        asset: assets[i],
-                                        selected: control.value.toInt() == i,
-                                        live: true,
-                                        onTap: () {
-                                          Debug.info('tap ${assets[i].id}');
-                                          if (control.value.toInt() != i) {
-                                            control.value = i.toDouble();
-                                            control.change(
-                                                source: ControlChangeSource.ui);
-                                          }
-                                        },
-                                      )))
-                        ])))
-          ])),
+                      controller: scrollController,
+                      slivers: [
+                        SliverGrid.builder(
+                          itemCount: assets.length,
+                          gridDelegate:
+                              SliverGridDelegateWithMinMaxCrossAxisExtent(
+                                maxCrossAxisCount: assets.length,
+                                minCrossAxisExtent: 160,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10,
+                                mainAxisExtent:
+                                    160 /
+                                    (aes.compositionSettings?.aspectRatio ??
+                                        16 / 9),
+                              ),
+                          itemBuilder: (_, i) => UIContextMenu(
+                            menu: [
+                              UIMenuItem(
+                                text: 'Remove camera input',
+                                icon: UIIcon.delete,
+                                color: Colors.redAccent,
+                                onTap: () {
+                                  aes.bus.fire(
+                                    RemoveAssetsEvent(
+                                      module: module,
+                                      assets: [assets[i]],
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                            child: AssetView(
+                              key: Key('${module.id}.${assets[i].id}'),
+                              moduleId: module.id,
+                              asset: assets[i],
+                              selected: control.value.toInt() == i,
+                              live: true,
+                              onTap: () {
+                                Debug.info('tap ${assets[i].id}');
+                                if (control.value.toInt() != i) {
+                                  control.value = i.toDouble();
+                                  control.change(
+                                    source: ControlChangeSource.ui,
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           const SizedBox(width: 5),
           Container(width: 0.5, color: ColorScheme.of(context).inversePrimary),
           const SizedBox(width: 10),
           SizedBox(
-              width: 338,
-              child: Column(children: [
+            width: 338,
+            child: Column(
+              children: [
                 SizedBox(
-                    height: 20,
-                    child: Row(children: [
-                      Text(module.name, style: TextTheme.of(context).bodySmall)
-                    ])),
+                  height: 20,
+                  child: Row(
+                    children: [
+                      Text(
+                        cname != null ? '${module.name} - $cname' : module.name,
+                        style: TextTheme.of(context).bodySmall,
+                      ),
+                    ],
+                  ),
+                ),
                 const SizedBox(height: 5),
                 ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: NativeView(
-                                moduleId: widget.moduleId,
-                                assetId: widget.moduleId)))),
+                  borderRadius: BorderRadius.circular(5),
+                  child: AspectRatio(
+                    aspectRatio: aes.compositionSettings?.aspectRatio ?? 16 / 9,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: NativeView(
+                        moduleId: widget.moduleId,
+                        assetId: widget.moduleId,
+                      ),
+                    ),
+                  ),
+                ),
                 const SizedBox(height: 5),
                 const Expanded(
-                    child: Center(
-                        child:
-                            Row(mainAxisSize: MainAxisSize.min, children: [])))
-              ])),
-          const SizedBox(width: 5)
-        ]));
+                  child: Center(
+                    child: Row(mainAxisSize: MainAxisSize.min, children: []),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 5),
+        ],
+      ),
+    );
   }
 
   Future<void> discoverCameras() async {
